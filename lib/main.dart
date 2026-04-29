30a3e972e951a5ed58a1b5303b0ba502834e3890
Mohamed <mm1665390@gmail.com>
2026-04-29 09:27:15 +0300

    initial commit

---
+++ lib/main.dart
@@ -0,0 +1,27 @@
+import 'package:ap/features/presentation/pages/home_page.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return ScreenUtilInit(
+      designSize: Size(375, 812),
+      minTextAdapt: true,
+      splitScreenMode: true,
+      builder: (context, child) {
+        return MaterialApp(
+          debugShowCheckedModeBanner: false,
+          initialRoute: "home",
+          routes: {"home": (BuildContext context) => HomePage()},
+        );
+      },
+    );
+  }
+}
